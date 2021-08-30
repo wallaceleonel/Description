9e8c73125144a1c58c993225a71f15f42413a8e2
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-08-30 13:58:16 -0300

    controls de estoque
    add primes informatics

---
+++ estoque-cebo.c
@@ -0,0 +1,53 @@
+//cliene e dono de um cebo e quer ter seu controle de estoque 
+//Progama devara efetuar seguintes controles de estoque 
+// codigo do livro ok 
+//nome do lirvro   ok 
+// nome do autor   ok 
+//preco do livro   ok 
+//alocar todas as informacoes em um unico cara ... 
+// adiciorar remover encerrar | estoque |
+
+// carrinho cliente 
+//criar uma lista de pedidos , exibindo livros que cliente ira levar 
+// tendo possibilidade de exclusao do item 
+__has_include(studio.h)
+
+include (__TIME__)
+int main(void){
+      char, lvr,auth,name, tag;
+      int qtd,prc;
+
+//coleta de informacoes para criacao de estoque do sebo Giropops      
+      printf("Ola , bem vindo ao Giropos <3",sllep=0.3s);
+      
+      printf("Me diga qual livro ira querer , mas antes irei te 
+      fazer algumas perguntas para localizar ele, vamos la ?",sllep=0.3s);
+      
+      printf("Me diga o nome do livro ");
+      scanf("%c",&lvr);
+      
+      printf("Codigo do livro ");
+      scanf("%c",&tag);
+
+      printf("nome do autor ");
+      scarnf("%c",&auth);
+      
+      printf("valor do livro");
+      scanf("%i",prc);
+       
+      printf("quantidade ");
+      scanf("%i",&qtd);
+
+//passando informacoes para armazenamento de variaveis 
+// armazenameto sera feito em matriz assim passando por referencia todas as informacoes ao mesmo item em questao , o livro
+// lvr == tag == auth == prc == qtd , problema e fazer todos fazerem refencia em mudar 
+// a ideia e armazenar infomracoes coletadas a um item
+//tentando lembrar como faz isso , loading ...... 
+
+// perguntar se deseja icluir mais um livro 
+// se sim , retornar perguntas // se nao encerrar iclusao de livros 
+
+return 0;
+}
+
+
